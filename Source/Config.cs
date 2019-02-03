3a1821dde1d9a19c163e9270174bbe2925aaad78
Dingo <oferdottan@gmail.com>
2019-02-03 02:15:00 +0200

    Initial upload

---
+++ Source/Config.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace MedkitHotkey
+{
+	/* Json serialization from:
+	 * https://docs.unity3d.com/Manual/JSONSerialization.html
+	 * 
+	 * Directory fetching snippet from:
+	 * https://stackoverflow.com/questions/52797/how-do-i-get-the-path-of-the-assembly-the-code-is-in */
+
+	[Serializable]
+	public class Config
+	{
+		public string FirstAidHotkey = "h";
+	}
+
+	public static class ConfigHandler
+	{
+		private static readonly string SettingsFileName = "settings.json";
+
+		private static string GetAssemblyDirectory
+		{
+			get
+			{
+				string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+				UriBuilder uri = new UriBuilder(codeBase);
+				string path = Uri.UnescapeDataString(uri.Path);
+				return Path.GetDirectoryName(path);
+			}
+		}
+
+		public static bool TryLoadConfig()
+		{
+			try
+			{
+				string settingsFilePath = Path.Combine(GetAssemblyDirectory, SettingsFileName);
+				string settingsAsJson = File.ReadAllText(settingsFilePath);
+				Config configFromJson = JsonUtility.FromJson<Config>(settingsAsJson);
+
+				// Missing some validation here...
+
+				Entry.ModConfig = configFromJson;
+			}
+
+			catch (Exception ex)
+			{
+				Debug.Log($"[MedkitHotkey] :: {ex.ToString()}");
+
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Source/Entry.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+
+namespace MedkitHotkey
+{
+	public static class Entry
+	{
+		public static Config ModConfig;
+
+		public static void Initialize()
+		{
+			if (ConfigHandler.TryLoadConfig())
+			{
+#if DEBUG
+				Debug.Log($"[MedkitHotkey] :: Current hotkey is '{ModConfig.FirstAidKeyCode}'");
+#endif
+
+				InitializeHarmony();
+
+#if DEBUG
+				Debug.Log($"[MedkitHotkey] :: Initialized Harmony");
+#endif
+			}
+
+			else
+			{
+				Debug.Log("[MedkitHotkey] :: Could not load hotkey from config file! Mod has not been enabled.");
+			}
+		}
+
+		public static void InitializeHarmony()
+		{
+			HarmonyInstance harmony = HarmonyInstance.Create("dingo.medkithotkey");
+
+#if DEBUG
+			HarmonyInstance.DEBUG = true;
+#endif
+
+			/* uGUI_QuickSlots.Update checks game state and does all of the necessary null checks.
+			 * This hotkey is only relevant when QuickSlots are relevant, so we check for input in the same method. */
+			MethodInfo handleQuickSlotsInput = AccessTools.Method(typeof(uGUI_QuickSlots), "HandleInput");
+
+			harmony.Patch(handleQuickSlotsInput, null, new HarmonyMethod(typeof(Entry), nameof(Patch_uGUI_QuickSlots_ListenForMedkit)), null);
+		}
+
+		public static void Patch_uGUI_QuickSlots_ListenForMedkit()
+		{
+			if (!IntroVignette.isIntroActive && Player.main.GetCanItemBeUsed() && Input.GetKeyDown(ModConfig.FirstAidHotkey))
+			{
+#if DEBUG
+				Debug.Log($"[MedkitHotkey] :: Pressed '{ModConfig.FirstAidKeyCode}', looking for medkit.");
+#endif
+
+				Inventory playerInventory = Inventory.main;
+
+				IList<InventoryItem> medkits = playerInventory.container.GetItems(TechType.FirstAidKit);
+
+				if (medkits != null)
+				{
+					playerInventory.UseItem(medkits.First());
+				}
+
+				else
+				{
+					ErrorMessage.AddWarning("No first aid kit in inventory!");
+				}
+			}
+		}
+	}
+}
