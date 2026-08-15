93ff9ce006186496d40710357e5b82d0b36d5736
Dingo <oferdottan@gmail.com>
2026-08-15 12:38:16 +0300

    utilize nautilus

--- Shared/Keybinds.cs
+++ Shared/Keybinds.cs
@@ -4,9 +4,11 @@ namespace MedkitHotkey
 {
     internal class Keybinds
     {
+        private static string MedkitHotkeyDefault => GameInputHandler.Paths.Keyboard.H;
+
         internal GameInput.Button FirstAidKey = EnumHandler.AddEntry<GameInput.Button>("MedkitHotkeyFirstAidKit")
                 .CreateInput("FirstAidKey".Translate(), "Tooltip_FirstAidKey".Translate())
-                .WithKeyboardBinding("<Keyboard>/h")
+                .WithKeyboardBinding(MedkitHotkeyDefault)
                 .WithControllerBinding("None")
                 .AvoidConflicts(GameInput.Device.Keyboard)
                 .WithCategory(ModPlugin.modName);
